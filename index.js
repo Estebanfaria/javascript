c3ff1e5afbaf1bc5c272b0bdfaec9179cbb0940b
Esteban Faria <tebifaria@gmail.com>
2022-08-29 15:16:35 -0300

    agrego nuevo ciclo

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
-let nombre = prompt ("Ingrese su nombre");
-alert ("Bienvenid@ " + nombre);
+// let nombre = prompt ("Ingrese su nombre");
+// alert ("Bienvenid@ " + nombre);
 
-let edad = parseInt (prompt ("¿Cuantos años tienes?")); 
+// let edad = parseInt (prompt ("¿Cuantos años tienes?")); 
 
-if (edad < 10){
-    alert ("Tenemos libros para niños");
-} else if (edad < 16) {
-    alert ("Tenemos libros para adolecentes");
-} else if (edad < 25){
-    alert ("Tenemos libros para jovenes");
-} else {
-    alert ("Tenemos libros para adultos");
+// if (edad < 10){
+//     alert ("Tenemos libros para niños, esperamos que lo disfrutes");
+// } else if (edad < 16) {
+//     alert ("Tenemos libros para adolescentes, esperamos que lo disfrutes");
+// } else if (edad < 25){
+//     alert ("Tenemos libros para jovenes, esperamos que lo disfrutes");
+// } else {
+//     alert ("Tenemos libros para adultos, esperamos que lo disfrutes");
+// }
+
+function categoria (nombre){
+    
+    switch (nombre) {
+        case "paz":
+        alert("Tenemos el tipo de libro esta buscando");
+        break;
+
+        case "gozo":
+        alert ("Tenemos el tipo de libro esta buscando");
+        break;
+
+        case "amor":
+        alert ("Tenemos el tipo de libro esta buscando");
+        break;
+
+        case "fe":
+        alert ("Tenemos el tipo de libro esta buscando");
+        break;
+
+        case "esperanza":
+        alert ("Tenemos el tipo de libro esta buscando");
+        break;
+
+        case "bibliografico":
+            alert ("Tenemos el tipo de libro esta buscando");
+            break;
+
+        default:
+            alert ("No tenemos el tipo de libro que esta buscando");
+            break;
+
+    }
 }
+
+let nombre = prompt("Ingrese el tipo de libro que esta buscando");
+categoria (nombre);

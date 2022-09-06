39fb8f70427496cefe2936178654624b6a2162ad
Esteban Faria <tebifaria@gmail.com>
2022-09-05 22:19:37 -0300

    termino arrays

--- index.js
+++ index.js
@@ -13,39 +13,45 @@
 //     alert ("Tenemos libros para adultos, esperamos que lo disfrutes");
 // }
 
-function categoria (nombre){
+// function categoria (nombre){
     
-    switch (nombre) {
-        case "paz":
-        alert("Tenemos el tipo de libro esta buscando");
-        break;
+//     switch (nombre) {
+//         case "paz":
+//         alert("Tenemos el tipo de libro esta buscando");
+//         break;
 
-        case "gozo":
-        alert ("Tenemos el tipo de libro esta buscando");
-        break;
+//         case "gozo":
+//         alert ("Tenemos el tipo de libro esta buscando");
+//         break;
 
-        case "amor":
-        alert ("Tenemos el tipo de libro esta buscando");
-        break;
+//         case "amor":
+//         alert ("Tenemos el tipo de libro esta buscando");
+//         break;
 
-        case "fe":
-        alert ("Tenemos el tipo de libro esta buscando");
-        break;
+//         case "fe":
+//         alert ("Tenemos el tipo de libro esta buscando");
+//         break;
 
-        case "esperanza":
-        alert ("Tenemos el tipo de libro esta buscando");
-        break;
+//         case "esperanza":
+//         alert ("Tenemos el tipo de libro esta buscando");
+//         break;
 
-        case "bibliografico":
-            alert ("Tenemos el tipo de libro esta buscando");
-            break;
+//         case "bibliografico":
+//             alert ("Tenemos el tipo de libro esta buscando");
+//             break;
 
-        default:
-            alert ("No tenemos el tipo de libro que esta buscando");
-            break;
+//         default:
+//             alert ("No tenemos el tipo de libro que esta buscando");
+//             break;
 
-    }
-}
+//     }
+// }
 
-let nombre = prompt("Ingrese el tipo de libro que esta buscando");
-categoria (nombre);
+// let nombre = prompt("Ingrese el tipo de libro que esta buscando");
+// categoria (nombre);}
+
+const libros = ["El dia en que fui crucificado","Este poder puede ser tuyo","Secretos del avivamiento en Argentina","Tres monarcas", "El nacimiento","En pos de lo supremo","Y todo lo que pidieres","Intercesor",];
+
+libros.push("La fe más allá de la razón");
+console.log(libros.length);
+console.log(libros);

3443afdd991a72c3a40de95ba48ebd95073902c6
Esteban Faria <tebifaria@gmail.com>
2022-09-11 21:26:26 -0300

    agrego funcion y objetos

--- index.js
+++ index.js
@@ -55,3 +55,33 @@ const libros = ["El dia en que fui crucificado","Este poder puede ser tuyo","Sec
 libros.push("La fe más allá de la razón");
 console.log(libros.length);
 console.log(libros);
+
+class biblioteca{
+    constructor(id, nombre, precio, img, cantidad){
+        this.id = id
+        this.nombre = nombre.toUpperCase()
+        this.precio = parseFloat(precio)
+        this.img = img
+        this.cantidad = cantidad
+    }
+    sumarIva(){
+        this.precio = this.precio * 1.21
+    }
+}
+
+const stockproductos = []
+
+stockproductos.push(new biblioteca("1","El dia en que fui crucificado",2500, "./multimedia/51sUzFNeLmL (1).jpg",1));
+stockproductos.push(new biblioteca("2","Este poder puede ser tuyo",3500, "./multimedia/este-poder_1.webpllls",4));
+stockproductos.push(new biblioteca("3","SecretoMArtes de un avivamiento en argentina",1500, "./multimedia/secretos-del-avivamiento-en-argentina.webp",22));
+stockproductos.push(new biblioteca("4","tres monarcas",900,"./multimedia/41GHUfpN11L._BO1,204,203,200_.jpg",5));
+stockproductos.push(new biblioteca("5","El nacimiento",4000,"./multimedia/El nacimiento.jpg",6));
+stockproductos.push(new biblioteca("6","En pos de lo supremo",1200,"./multimedia/en pos de lo supremo.jpg",5));
+stockproductos.push(new biblioteca("7","Y todo lo que pidieres",900,"./multimedia/y todo lo que pidieres.webp",10));
+stockproductos.push(new biblioteca("8","Intercesor",1600,"./multimedia/el intercesor.jpg",30));
+stockproductos.push(new biblioteca("9","La fe mas alla de la razon",2100,"./multimedia/la fe.jpg",25));
+
+
+
+
+

f34bc444f1c10c8e166d01aa28c702d3b18b88d8
Esteban Faria <tebifaria@gmail.com>
2022-09-12 08:02:41 -0300

    arrgelo js y agrego evento

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
-const productos = [ 
-    { id: 1, nombre: "El intercesor", precio: 1000, tapa: "./multimedia/el intercesor.jpg" },    
-    { id: 2, nombre: "El dia que fui crucificado", precio: 1500, tapa: "./multimedia/51sUzFNeLmL (1).jpg" },   
-    { id: 3, nombre: "Perfil de tres monarcas", precio: 1900,tapa:"./multimedia/41GHUfpN11L._BO1,204,203,200_.jpg" },
-    { id: 4, nombre: "El nacimiento", precio: 2100,tapa:"./multimedia/El nacimiento.jpg" },
-    { id: 5, nombre: "En pos de lo supremo", precio: 2080,tapa:"./multimedia/en pos de lo supremo.jpg" },
-    { id: 6, nombre: "Este poder puede ser tuyo", precio: 2500,tapa:"./multimedia/este-poder_1.webp" },
+const libros = [
+    {nombre:"El dia en que fui crucificado",precio:5000 },
+    {nombre:"Este poder puede ser tuyo",precio:2000},
+    {nombre:"Secretos del avivamiento en Argentina",precio:3000},
+    {nombre:"Tres monarcas",precio:1000},
+    {nombre:"El nacimiento",precio:7000},
+    {nombre:"En pos de lo supremo",precio:2200},
+    {nombre:"Y todo lo que pidieres",precio:1000},
+    {nombre:"Intercesor",precio:1200},
+    {nombre:"La fe más allá de la razón",precio:3500},
+]
+
+
+
+const formulario = document.querySelector ("#formulario");
+const boton = document.querySelector ("#boton");
+const resultado = document.querySelector("#resultado"); 
+
+const filtrar = ()=>{
+    // console.log(formulario.value);
+    resultado.innerHTML = ""; 
+
+    const texto = formulario.value.toLowerCase();
     
-];  
+    for (let producto of libros){
+        let nombre = producto.nombre.toLowerCase();
+            if(nombre.indexOf(texto) !== -1){
+                resultado.innerHTML += `<li>Nombre:${producto.nombre}</li>
+                                        <li>Precio:${producto.precio}</li>`
+            }
+    }
 
-let contenedor = document.getElementById ("contenedor");
-let precio = parseInt(prompt("Ingrese el precio minimo"));
-
-
-let productosFiltrados = productos.filter(item => item.precio > precio);
-
-for (const producto of productosFiltrados) {
-    let div = document.createElement("div");
-    div.innerHTML = `<h3> ID: ${producto.id}</h3>
-                    <p> Producto: ${producto.nombre}</p>
-                    <b> Precio: ${producto.precio}</b>
-                    <img src="${producto.tapa}" alt="">`; 
-                    
-
-    contenedor.append(div);
+    if(resultado.innerHTML === ""){
+        resultado.innerHTML += `<p>Producto no encontrado...</p>`;
+    }
 }
+
+boton.addEventListener("click", filtrar);
+formulario.addEventListener("keyup", filtrar);
+
+filtrar();
+
+
+
 
 
 
